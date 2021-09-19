74dafdea7f9f722b266452a5648fa609665dd66c
Arda <arda.dizdaroglu@teamdefinex.com>
2021-09-19 16:31:54 +0300

    style restaurant list

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
+import 'package:auto_animated/auto_animated.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_eatit_app/model/restaurant_model.dart';
 import 'package:flutter_eatit_app/strings/main_strings.dart';
 import 'package:flutter_eatit_app/view_model/main_view_model_imp.dart';
+import 'package:flutter_eatit_app/widgets/common/common_widgets.dart';
+import 'package:flutter_eatit_app/widgets/main/main_widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-Future<void> main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   FirebaseApp app = await Firebase.initializeApp();
-  runApp(MyApp(app:app));
+  runApp(MyApp(app: app));
 }
 
 class MyApp extends StatelessWidget {
@@ -22,10 +25,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(app:app),
+      home: MyHomePage(app: app),
     );
   }
 }
@@ -33,30 +35,58 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   final FirebaseApp app;
   final viewModel = MainViewModelImp();
+
   MyHomePage({required this.app});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(restaurantText,
-        style: GoogleFonts.jetBrainsMono(
-            fontWeight: FontWeight.w900, color: Colors.black),),
-      backgroundColor: Colors.white,
-      elevation: 10,),
+      appBar: AppBar(
+        title: Text(
+          restaurantText,
+          style: GoogleFonts.jetBrainsMono(
+              fontWeight: FontWeight.w900, color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 10,
+      ),
       body: FutureBuilder(
         future: viewModel.displayRestaurantList(),
-        builder: (context,snapshot){
-          if(snapshot.connectionState == ConnectionState.waiting)
-            return Center(child: CircularProgressIndicator(),);
-          else
-            {
-              var lst = snapshot.data as List<RestaurantModel>;
-              return ListView.builder(
-                  itemCount: lst.length,
-                  itemBuilder: (context, index){
-                    return Text(lst[index].name);
-                  });
-            }
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting)
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          else {
+            var lst = snapshot.data as List<RestaurantModel>;
+            return Container(
+              margin: const EdgeInsets.only(top: 10),
+              child: LiveList(
+                showItemDuration: Duration(milliseconds: 350),
+                showItemInterval: Duration(milliseconds: 150),
+                reAnimateOnVisibility: true,
+                scrollDirection: Axis.vertical,
+                itemCount: lst.length,
+                itemBuilder: animationItemBuilder((index) => InkWell(
+                      onTap: () {},
+                      child: Container(
+                        width: double.infinity,
+                        height: MediaQuery.of(context).size.height / 2.5 * 1.1,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            RestaurantImageWidget(
+                                imageUrl: lst[index].imageUrl),
+                            RestaurantInfoWidget(
+                                name: lst[index].name,
+                                address: lst[index].address)
+                          ],
+                        ),
+                      ),
+                    )),
+              ),
+            );
+          }
         },
       ),
     );
